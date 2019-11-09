5c6fa68133cbe7c5a9ed289ec7c0e8527c26c014
edmond <644568491@qq.com>
2019-11-10 07:26:39 +0800

    initial commit

---
+++ src/components/Planet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-bind:style="{ width: radius + 'px'}">
+        {{name}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'Planet',
+    props: {
+        name: String,
+        radius: Number
+    }
+}
+</script>
+<style lang="sass" scoped>
+$box-model: border-box;
+div {
+    box-sizing: $box-model;
+    border-radius: 50%;
+}
+</style>
